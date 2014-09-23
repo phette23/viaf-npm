84953777da17bea9c63b462d7d95b945944fce85
phette23 <phette23@gmail.com>
2014-09-22 19:24:13 -0700

    use sepia for caching HTTP responses during testing

--- test/read.js
+++ test/read.js
@@ -1,14 +1,6 @@
+/*jshint unused:false */
 var viaf = require('../viaf')
-var dns = require('dns')
-
-// check if we can even connect to viaf.org
-// exit if not; tests rely on HTTP
-dns.resolve('viaf.org', function (err) {
-    if (err) {
-        console.log('Unable to resolve viaf.org address. Are you connected to the interwebs?');
-        process.exit();
-    }
-})
+var sepia = require('sepia')
 
 exports['Successful HTTP Request & Response'] = function (test) {
     viaf.read('102333412', function (err, resp, body) {
