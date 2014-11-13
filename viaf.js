a072f47016395a923d6fc65e715d87c7d8ddd093
phette23 <phette23@gmail.com>
2014-11-12 20:28:24 -0800

    add in VIAF dataType maps

--- viaf.js
+++ viaf.js
@@ -10,12 +10,39 @@ viaf.stems = {
     autosuggest: '/viaf/AutoSuggest?query='
 };
 // default request options
+// only safe default types are html, rss, & xml
+viaf.defaultDataType = 'xml';
 viaf.requestOptions = {
     headers: {
         'Accept-Charset': 'UTF-8'
     }
 };
 
+// VIAF's many data types, all listed on
+// https://www.oclc.org/developer/develop/web-services/viaf/authority-cluster.en.html
+// keep these properties all lowercase without hyphens
+// parameters will be lowercased & have hyphens stripped before being compared against
+
+// appended onto the end of data (read) request URLs
+viaf.dataResponseTypes = {
+    'html' : '/', // VIAF's default so just requires a trailing slash
+    'xml' : '/viaf.xml',
+    'json' : '/justlinks.json', // just links
+    'rdf' : '/rdf.xml',
+    'jsonld' : '/viaf.jsonld',
+    'rss' : '/rss.xml',
+    'marcxml' : '/marc21.xml',
+    'marchtml' : '/marc21.html', // basically same as above, forces an HTML download
+    'unimarcxml' : '/unimarc.xml',
+    'unimarchtml' : '/unimarc.html' // basically same as above, forces an HTML download
+};
+
+// used in SRU HTTP headers
+viaf.searchResponseTypes = {
+    'html' : 'text/html',
+    'rss' : 'application/rss+xml',
+    'xml' : 'text/xml',
+};
 // helper function
 viaf.url = function (type) {
     // each time request is made, use viaf.requestOptions
